877ae140a56b6a2c819988f42a2a24e883b01e18
benxta4424 <benxtaben@gmail.com>
2025-01-25 02:13:40 +0200

    completing all the routes in the kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,55 @@
 class KittensController < ApplicationController
+  def index
+    @kittens=Kitten.all
+  end
+
+  def new
+    @kitten=Kitten.new(kitten_params)
+  end
+
+  def create
+    @kitten=Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:creation_success]="Sucesfully created a kitten!"
+      redirect_to kitten_path(@kitten.id)
+    else
+      flash[:unsucessful_creation]="Unsuccesful in the creation of the kitten! Try again!"
+      render :new
+    end
+  end
+
+  def show
+    @kitten=Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten=Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten=Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:update_success]="Successfully updated the kitten!"
+      redirect_to kitten_path(@kitten.id)
+    else
+      flash[:unsuccessful_update]="Unsuccessful in updating the kitten! Try again!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten=Kitten.find(params[:id])
+    @kitten.destroy
+
+    flash[:succesfully_deleted]="Kitten deleted succesfully!"
+    redirect_to root_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kittens).permit(:name, :age, :cuteness, :softness)
+  end
 end
